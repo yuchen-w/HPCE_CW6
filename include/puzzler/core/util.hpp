63a75a11a9494ff58d5924cae6f78b06efdf8487
Yuchen Wang <yuchen_wng@hotmail.com>
2015-03-10 16:01:43 +0000

    Initial commit
    Compiles on VS2013 and cygwin via make

--- include/puzzler/core/util.hpp
+++ include/puzzler/core/util.hpp
@@ -20,12 +20,12 @@
 #include <unistd.h>
 namespace puzzler
 {
-  class WithBinaryIO
-  {
-  public:
-    WithBinaryIO()
-    {}
-  };
+	class WithBinaryIO
+	{
+	public:
+		WithBinaryIO()
+		{}
+	};
 }
 #else
 // http://stackoverflow.com/questions/341817/is-there-a-replacement-for-unistd-h-for-windows-visual-c
@@ -50,25 +50,25 @@ namespace puzzler
 
 namespace puzzler
 {
-  class WithBinaryIO
-  {
-  private:
-    int m_stdinPrev, m_stdoutPrev;
-  public:
-    WithBinaryIO()
-    {
-      fflush(_fileno(stdout));
-      m_stdinPrev=_setmode(_fileno(stdin), _O_BINARY);
-      m_stdoutPrev=_setmode(_fileno(stdout), _O_BINARY);
-    }
+	class WithBinaryIO
+	{
+	private:
+		int m_stdinPrev, m_stdoutPrev;
+	public:
+		WithBinaryIO()
+		{
+			fflush((FILE*)_fileno(stdout));
+			m_stdinPrev = _setmode(_fileno(stdin), _O_BINARY);
+			m_stdoutPrev = _setmode(_fileno(stdout), _O_BINARY);
+		}
 
-    ~WithBinaryIO()
-    {
-      fflush(_fileno(stdout));
-      m_stdinPrev=_setmode(_fileno(stdin), _O_BINARY);
-      m_stdoutPrev=_setmode(_fileno(stdout), _O_BINARY);
-    }
-  };
+		~WithBinaryIO()
+		{
+			fflush((FILE*)_fileno(stdout));
+			m_stdinPrev = _setmode(_fileno(stdin), _O_BINARY);
+			m_stdoutPrev = _setmode(_fileno(stdout), _O_BINARY);
+		}
+	};
 }
 #endif
 
@@ -78,50 +78,50 @@ namespace puzzler
 
 
 
-  /*
-    Times are represented using a 64 bit number representing nano-seconds
-    from unix epoch (see time() for epoch definition).
-  */
-  typedef uint64_t timestamp_t;
+	/*
+	Times are represented using a 64 bit number representing nano-seconds
+	from unix epoch (see time() for epoch definition).
+	*/
+	typedef uint64_t timestamp_t;
 
 #if !defined(__CYGWIN__) && defined(_WIN32) || defined(_WIN64)
-  inline timestamp_t now()
-  {
-    FILETIME ft;
-    GetSystemTimeAsFileTime(&ft);
-    uint64_t tt = ft.dwHighDateTime;
-    tt = (tt<<32) + ft.dwLowDateTime;
-    tt *=100;
-    tt -= 11644473600000000000ULL;
-    return tt;;
-  }
+	inline timestamp_t now()
+	{
+		FILETIME ft;
+		GetSystemTimeAsFileTime(&ft);
+		uint64_t tt = ft.dwHighDateTime;
+		tt = (tt << 32) + ft.dwLowDateTime;
+		tt *= 100;
+		tt -= 11644473600000000000ULL;
+		return tt;;
+	}
 #else
-  inline timestamp_t now()
-  {
-    struct timespec ts;
-    if(0!=clock_gettime(CLOCK_REALTIME, &ts))
-      throw std::runtime_error("puzzle::now() - Couldn't read time.");
-    return uint64_t(1e9*ts.tv_sec+ts.tv_nsec);
-  }
+	inline timestamp_t now()
+	{
+		struct timespec ts;
+		if (0 != clock_gettime(CLOCK_REALTIME, &ts))
+			throw std::runtime_error("puzzle::now() - Couldn't read time.");
+		return uint64_t(1e9*ts.tv_sec + ts.tv_nsec);
+	}
 #endif
 
-  template<class TOut, class TIn>
-  static TOut *As(TIn *in)
-  {
-    TOut *tmp=dynamic_cast<TOut *>(in);
-    if(tmp==NULL)
-      throw std::runtime_error("Puzzle::As - Couldn't convert pointer to requested type");
-    return tmp;
-  }
+	template<class TOut, class TIn>
+	static TOut *As(TIn *in)
+	{
+		TOut *tmp = dynamic_cast<TOut *>(in);
+		if (tmp == NULL)
+			throw std::runtime_error("Puzzle::As - Couldn't convert pointer to requested type");
+		return tmp;
+	}
 
-  template<class TOut, class TIn>
-  static const TOut *As(const TIn *in)
-  {
-    const TOut *tmp=dynamic_cast<const TOut*>(in);
-    if(tmp==NULL)
-      throw std::runtime_error("Puzzle::As - Couldn't convert pointer to requested type");
-    return tmp;
-  }
+	template<class TOut, class TIn>
+	static const TOut *As(const TIn *in)
+	{
+		const TOut *tmp = dynamic_cast<const TOut*>(in);
+		if (tmp == NULL)
+			throw std::runtime_error("Puzzle::As - Couldn't convert pointer to requested type");
+		return tmp;
+	}
 
 
 }; // puzzler
