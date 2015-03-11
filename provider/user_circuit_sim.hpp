a0921a0b5f8ffc0b128a5671f2253971094e7a25
Yuchen Wang <yuchen_wng@hotmail.com>
2015-03-11 22:29:43 +0000

    tbb::task_group implemented
    Still to do:
    tbb::parfor

--- provider/user_circuit_sim.hpp
+++ provider/user_circuit_sim.hpp
@@ -2,11 +2,31 @@
 #define user_circuit_sim_hpp
 
 #include "puzzler/puzzles/circuit_sim.hpp"
+#include "tbb/task_group.h"
 //#include "puzzler\core\log.hpp"
 
 class CircuitSimProvider
   : public puzzler::CircuitSimPuzzle
 {
+private:
+	bool calcSrc_tbb(unsigned src, const std::vector<bool> &state, const puzzler::CircuitSimInput *input) const
+	{
+		if (src < input->flipFlopCount){
+			return state.at(src);
+		}
+		else{
+			unsigned nandSrc = src - input->flipFlopCount;
+			tbb::task_group calcSrc_group;
+			bool a, b;
+			auto calcSrc_first = [&]() {a = calcSrc_tbb(input->nandGateInputs.at(nandSrc).first, state, input); };
+			auto calcSrc_second = [&]() {b = calcSrc_tbb(input->nandGateInputs.at(nandSrc).second, state, input); };
+			calcSrc_group.run(calcSrc_first);
+			calcSrc_group.run(calcSrc_second);
+			calcSrc_group.wait();
+			return !(a&&b);
+		}
+	}
+
 public:
   CircuitSimProvider()
   {}
@@ -26,7 +46,7 @@ public:
 
 		//state = next(state, input);
 		for (unsigned j = 0; j<state.size(); j++){
-			state_temp[j] = calcSrc(input->flipFlopInputs[j], state, input);
+			state_temp[j] = calcSrc_tbb(input->flipFlopInputs[j], state, input);	//Optimise this function?
 		}
 		state = state_temp;	//ToDo:swap pointers here
 
