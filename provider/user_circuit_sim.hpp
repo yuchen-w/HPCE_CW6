cc36f7b88eb4dfcbfadfa7163ed2d8318cfe72a2
Yuchen Wang <yuchen_wng@hotmail.com>
2015-03-11 20:10:49 +0000

    Migrated CircuitSim ref code over ready for user optimisation
    To do:
    TBB of double for loop

--- provider/user_circuit_sim.hpp
+++ provider/user_circuit_sim.hpp
@@ -2,7 +2,7 @@
 #define user_circuit_sim_hpp
 
 #include "puzzler/puzzles/circuit_sim.hpp"
-
+//#include "puzzler\core\log.hpp"
 
 class CircuitSimProvider
   : public puzzler::CircuitSimPuzzle
@@ -16,7 +16,32 @@ public:
 		       const puzzler::CircuitSimInput *input,
 		       puzzler::CircuitSimOutput *output
 		       ) const override {
-    ReferenceExecute(log, input, output);
+	log->LogVerbose("About to start running clock cycles (total = %d", input->clockCycles);
+	std::vector<bool> state = input->inputState;
+	std::vector<bool> state_temp(state.size());
+
+	//TODO: OPTMISE
+	for (unsigned i = 0; i<input->clockCycles; i++){
+		log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
+
+		//state = next(state, input);
+		for (unsigned j = 0; j<state.size(); j++){
+			state_temp[j] = calcSrc(input->flipFlopInputs[j], state, input);
+		}
+		state = state_temp;	//ToDo:swap pointers here
+
+		// The weird form of log is so that there is little overhead
+		// if logging is disabled
+		log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
+			for (unsigned i = 0; i<state.size(); i++){
+				dst << state[i];
+			}
+		});
+	}
+
+	log->LogVerbose("Finished clock cycles");
+
+	output->outputState = state;
   }
 
 };
