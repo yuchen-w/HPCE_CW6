57783cac5ac638f515bfa0d24660687835cba5b4
Yuchen Wang <yuchen_wng@hotmail.com>
2015-03-12 18:47:25 +0000

    TBB runs slower than reference, so code no longer runs TBB for circuit_sim
    Also copied over ReferenceExecute for user_option_explicit

--- provider/user_circuit_sim.hpp
+++ provider/user_circuit_sim.hpp
@@ -3,12 +3,21 @@
 
 #include "puzzler/puzzles/circuit_sim.hpp"
 #include "tbb/task_group.h"
-//#include "puzzler\core\log.hpp"
+#include "tbb/parallel_for.h"
+#include "tbb/blocked_range2d.h"
 
 class CircuitSimProvider
-  : public puzzler::CircuitSimPuzzle
+	: public puzzler::CircuitSimPuzzle
 {
 private:
+	std::vector<int> vector_boolToInt(std::vector<bool> source) {
+		std::vector<int> return_vector;
+		for (bool value : source) {
+			return_vector.push_back(value ? 1 : 0);
+		}
+		return return_vector;
+	}
+
 	bool calcSrc_tbb(unsigned src, const std::vector<bool> &state, const puzzler::CircuitSimInput *input) const
 	{
 		if (src < input->flipFlopCount){
@@ -27,42 +36,175 @@ private:
 		}
 	}
 
-public:
-  CircuitSimProvider()
-  {}
+	std::vector<bool> next_tbb(const std::vector<bool> &state, const puzzler::CircuitSimInput *input) const
+	{
+		std::vector<bool> res(state.size());
+		/*for (unsigned i = 0; i<res.size(); i++){
+		res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		}*/
 
-  virtual void Execute(
-		       puzzler::ILog *log,
-		       const puzzler::CircuitSimInput *input,
-		       puzzler::CircuitSimOutput *output
-		       ) const override {
-	log->LogVerbose("About to start running clock cycles (total = %d", input->clockCycles);
-	std::vector<bool> state = input->inputState;
-	std::vector<bool> state_temp(state.size());
-
-	//TODO: OPTMISE parfor and blocked_range2d 
-	for (unsigned i = 0; i<input->clockCycles; i++){
-		log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
-
-		//state = next(state, input);
-		for (unsigned j = 0; j<state.size(); j++){
-			state_temp[j] = calcSrc_tbb(input->flipFlopInputs[j], state, input);	//Optimise this function?
-		}
-		state = state_temp;	//ToDo:swap pointers here
-
-		// The weird form of log is so that there is little overhead
-		// if logging is disabled
-		log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
-			for (unsigned i = 0; i<state.size(); i++){
-				dst << state[i];
+		int K = 10;
+		typedef tbb::blocked_range<unsigned> my_range_t;
+		my_range_t range(0, res.size(), K);
+		auto f = [&](const my_range_t &chunk)
+		{
+			for (unsigned i = chunk.begin(); i != chunk.end(); i++)
+			{
+				res[i] = calcSrc(input->flipFlopInputs[i], state, input);
 			}
-		});
+		};
+		tbb::parallel_for(range, f, tbb::simple_partitioner());
+
+		/*unsigned size = res.size();
+		auto f = [&](unsigned i)
+		{
+			res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		};
+		tbb::parallel_for(0u, size, f);*/
+
+
+
+		/*for (unsigned i = 0; i < res.size(); i++)
+		{
+			res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		}*/
+
+		return res;
+	}
+	
+
+	int calcSrc_int(unsigned src, const std::vector<int> &state, const puzzler::CircuitSimInput *input) const
+	{
+		if (src < input->flipFlopCount){
+			return state.at(src);
+		}
+		else{
+			unsigned nandSrc = src - input->flipFlopCount;
+			bool a = !!calcSrc_int(input->nandGateInputs.at(nandSrc).first, state, input);
+			bool b = !!calcSrc_int(input->nandGateInputs.at(nandSrc).second, state, input);
+			return !!!(a&&b);
+		}
 	}
 
-	log->LogVerbose("Finished clock cycles");
+	std::vector<int> next_int(const std::vector<int> &state, const puzzler::CircuitSimInput *input) const
+	{
+		std::vector<int> res(state.size());
+		for (unsigned i = 0; i<res.size(); i++){
+			res[i] = calcSrc_int(input->flipFlopInputs[i], state, input);
+		}
 
-	output->outputState = state;
-  }
+		return res;
+	}
+public:
+	CircuitSimProvider()
+	{}
+
+	virtual void Execute(
+		puzzler::ILog *log,
+		const puzzler::CircuitSimInput *input,
+		puzzler::CircuitSimOutput *output
+		) const override {
+		log->LogVerbose("About to start running clock cycles (total = %d", input->clockCycles);
+		std::vector<bool> state = input->inputState;
+
+		bool conversion = false;
+		bool parallel = false;
+		if (conversion == false)
+		{
+			if (parallel == true)
+			{
+				int K = 10;
+				typedef tbb::blocked_range<unsigned> my_range_t;
+				my_range_t range(0, input->clockCycles, K);
+				auto f = [&](const my_range_t &chunk)
+				{
+					for (unsigned i = chunk.begin(); i != chunk.end(); i++)
+					{
+						log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
+
+						state = next(state, input);
+
+						// The weird form of log is so that there is little overhead
+						// if logging is disabled
+						log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
+							for (unsigned j = 0; i < state.size(); j++){
+								dst << state[j];
+							}
+						});
+					}
+				};
+				tbb::parallel_for(range, f, tbb::simple_partitioner());
+				//tbb::parallel_for(0u, input->clockCycles, f);
+			}
+			else
+			{
+				for (unsigned i = 0; i<input->clockCycles; i++){
+					log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
+
+					state = next_tbb(state, input);
+
+					// The weird form of log is so that there is little overhead
+					// if logging is disabled
+					log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
+						for (unsigned i = 0; i<state.size(); i++){
+							dst << state[i];
+						}
+					});
+				}
+			}
+		}
+		else
+		{
+			std::vector<int> state_int;
+			for (bool value : state) {
+				state_int.push_back(value ? 1 : 0);
+			}
+			//conversion taken from reddit because of issues in VS with vector<bool>
+
+			//int K = 10;
+			//typedef tbb::blocked_range<unsigned> my_range_t;
+			//my_range_t range(0, input->clockCycles, K);
+			//auto f = [&](const my_range_t &chunk)
+			//{
+			//	for (unsigned i = chunk.begin(); i != chunk.end(); i++)
+			//	{
+			//		log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
+
+			//		state_int = next_int(state_int, input);
+
+			//		// The weird form of log is so that there is little overhead
+			//		// if logging is disabled
+			//		log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
+			//			for (unsigned j = 0; i < state.size(); j++){
+			//				dst << state[j];
+			//			}
+			//		});
+			//	}
+			//};
+			//tbb::parallel_for(range, f, tbb::simple_partitioner());
+
+			for (unsigned i = 0; i<input->clockCycles; i++){
+				log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
+
+				state_int = next_int(state_int, input);
+
+				// The weird form of log is so that there is little overhead
+				// if logging is disabled
+				log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
+					for (unsigned i = 0; i<state_int.size(); i++){
+						dst << state_int[i];
+					}
+				});
+			}
+
+			for (int i : state_int) {
+				state.at(i)=!!state_int.at(i);
+			}
+		}
+		log->LogVerbose("Finished clock cycles");
+
+		output->outputState = state;
+	}
 
 };
 
--- provider/user_option_explicit.hpp
+++ provider/user_option_explicit.hpp
@@ -15,7 +15,45 @@ public:
 		       const puzzler::OptionExplicitInput *input,
 		       puzzler::OptionExplicitOutput *output
 		       ) const override {
-    ReferenceExecute(log, input, output);
+	  int n = input->n;
+	  double u = input->u, d = input->d;
+
+	  log->LogInfo("Params: u=%lg, d=%lg, wU=%lg, wM=%lg, wD=%lg", input->u, input->d, input->wU, input->wM, input->wD);
+
+	  std::vector<double> state(n * 2 + 1);
+	  double vU = input->S0, vD = input->S0;
+	  state[input->n] = (std::max)(vU - input->K, 0.0);
+	  for (int i = 1; i <= n; i++){
+		  vU = vU*u;
+		  vD = vD*d;
+		  state[n + i] = (std::max)(vU - input->K, 0.0);
+		  state[n - i] = (std::max)(vD - input->K, 0.0);
+	  }
+
+	  double wU = input->wU, wD = input->wD, wM = input->wM;
+	  for (int t = n - 1; t >= 0; t--){
+		  std::vector<double> tmp = state;
+
+		  vU = input->S0, vD = input->S0;
+		  for (int i = 0; i<n; i++){
+			  double vCU = wU*state[n + i + 1] + wM*state[n + i] + wD*state[n + i - 1];
+			  double vCD = wU*state[n - i + 1] + wM*state[n - i] + wD*state[n - i - 1];
+			  vCU = (std::max)(vCU, vU - input->K);
+			  vCD = (std::max)(vCD, vD - input->K);
+			  tmp[n + i] = vCU;
+			  tmp[n - i] = vCD;
+
+			  vU = vU*u;
+			  vD = vD*d;
+		  }
+
+		  state = tmp;
+	  }
+
+	  output->steps = n;
+	  output->value = state[n];
+
+	  log->LogVerbose("Priced n=%d, S0=%lg, K=%lg, r=%lg, sigma=%lg, BU=%lg : value=%lg", n, input->S0, input->K, input->r, input->sigma, input->BU, output->value);
   }
 
 };
