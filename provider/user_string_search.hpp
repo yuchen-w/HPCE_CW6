7b07490c08a0d8d350da6ae7a641fdfe7e6281e2
Yuchen Wang <yuchen_wng@hotmail.com>
2015-03-12 21:43:35 +0000

    Commencing work on string_search. Copied implementation of Reference

--- provider/user_string_search.hpp
+++ provider/user_string_search.hpp
@@ -16,7 +16,36 @@ public:
 		       puzzler::StringSearchOutput *output
 		       ) const override
   {
-    ReferenceExecute(log, input, output);
+	  std::vector<uint32_t> histogram(input->patterns.size(), 0);
+
+	  std::string data = MakeString(input->stringLength, input->seed);
+
+	  unsigned i = 0;
+	  while (i < input->stringLength){
+
+		  for (unsigned p = 0; p<input->patterns.size(); p++){
+			  unsigned len = Matches(data, i, input->patterns[p]);
+			  if (len>0){
+				  log->Log(puzzler::Log_Debug, [&](std::ostream &dst){
+					  dst << "  Found " << input->patterns.at(p) << " at offset " << i << ", match=" << data.substr(i, len);
+				  });
+				  histogram[p]++;
+				  i += len - 1;
+				  break;
+			  }
+		  }
+
+		  ++i;
+	  }
+
+	  for (unsigned i = 0; i<histogram.size(); i++){
+		  log->Log(puzzler::Log_Debug, [&](std::ostream &dst){
+			  dst << input->patterns[i].c_str() << " : " << histogram[i];
+		  });
+	  }
+
+
+	  output->occurences = histogram;
   }
 
 };
