d0b193359ae36619dcd7d4927d250bc265edbce1
Yuchen Wang <yuchen_wng@hotmail.com>
2015-03-16 22:31:53 +0000

    Working circuit_sim for testing on AWS

--- provider/user_circuit_sim.hpp
+++ provider/user_circuit_sim.hpp
@@ -7,17 +7,36 @@
 #include "tbb/task_group.h"
 #include "tbb/parallel_for.h"
 #include "tbb/blocked_range2d.h"
+#include "tbb/concurrent_vector.h"
 
 class CircuitSimProvider
 	: public puzzler::CircuitSimPuzzle
 {
 private:
-	std::vector<int> vector_boolToInt(std::vector<bool> source) {
-		std::vector<int> return_vector;
-		for (bool value : source) {
-			return_vector.push_back(value ? 1 : 0);
+	bool calcSrc(unsigned src, const std::vector<bool> &state, const puzzler::CircuitSimInput *input) const
+	{
+		if (src < input->flipFlopCount){
+			return state.at(src);
 		}
-		return return_vector;
+		else{
+			unsigned nandSrc = src - input->flipFlopCount;
+			bool a = calcSrc(input->nandGateInputs.at(nandSrc).first, state, input);
+			bool b = calcSrc(input->nandGateInputs.at(nandSrc).second, state, input);
+			return !(a&&b);
+		}
+	}
+
+	char calcSrc(unsigned src, const std::vector<char> &state, const puzzler::CircuitSimInput *input) const
+	{
+		if (src < input->flipFlopCount){
+			return state.at(src);
+		}
+		else{
+			unsigned nandSrc = src - input->flipFlopCount;
+			bool a = calcSrc(input->nandGateInputs.at(nandSrc).first, state, input);
+			bool b = calcSrc(input->nandGateInputs.at(nandSrc).second, state, input);
+			return !(a&&b);
+		}
 	}
 
 	bool calcSrc_tbb(unsigned src, const std::vector<bool> &state, const puzzler::CircuitSimInput *input, unsigned K) const
@@ -45,75 +64,108 @@ private:
 		}
 	}
 
-	std::vector<bool> next_tbb(const std::vector<bool> &state, const puzzler::CircuitSimInput *input) const
-	{
-		std::vector<bool> res(state.size());
-		/*for (unsigned i = 0; i<res.size(); i++){
-		res[i] = calcSrc(input->flipFlopInputs[i], state, input);
-		}*/
-
-		//if (res.size() < 1000){
-			int K = 1000;
-			typedef tbb::blocked_range<unsigned> my_range_t;
-			my_range_t range(0, res.size(), K);
-			auto f = [&](const my_range_t &chunk)
-			{
-				for (unsigned i = chunk.begin(); i != chunk.end(); i++)
-				{
-					res[i] = calcSrc_tbb(input->flipFlopInputs[i], state, input, res.size());
-				}
-			};
-			tbb::parallel_for(range, f, tbb::simple_partitioner());
-		//}
-		//else
-		//{
-		//	for (unsigned i = 0; i < res.size(); i++)
-		//	{
-		//		unsigned K = res.size();
-		//		res[i] = calcSrc_tbb(input->flipFlopInputs[i], state, input, K/4);
-		//		//res[i] = calcSrc(input->flipFlopInputs[i], state, input);
-		//	}
-		//}
-		/*unsigned size = res.size();
-		auto f = [&](unsigned i)
-		{
-			res[i] = calcSrc(input->flipFlopInputs[i], state, input);
-		};
-		tbb::parallel_for(0u, size, f);*/
-
-
-
-		/*for (unsigned i = 0; i < res.size(); i++)
-		{
-			res[i] = calcSrc(input->flipFlopInputs[i], state, input);
-		}*/
-
-		return res;
-	}
-	
-
-	int calcSrc_int(unsigned src, const std::vector<int> &state, const puzzler::CircuitSimInput *input) const
+	char calcSrc_tbb(unsigned src, const std::vector<char> &state, const puzzler::CircuitSimInput *input, unsigned K) const
 	{
 		if (src < input->flipFlopCount){
 			return state.at(src);
 		}
 		else{
+			//TODO: Parallel across number of processors to reduce overhead
 			unsigned nandSrc = src - input->flipFlopCount;
-			bool a = !!calcSrc_int(input->nandGateInputs.at(nandSrc).first, state, input);
-			bool b = !!calcSrc_int(input->nandGateInputs.at(nandSrc).second, state, input);
-			return !!!(a&&b);
+
+			if (K < 100)
+				return calcSrc(input->nandGateInputs.at(nandSrc).first, state, input);
+			else
+			{
+				tbb::task_group calcSrc_group;
+				bool a, b;
+				auto calcSrc_first = [&]() {a = calcSrc_tbb(input->nandGateInputs.at(nandSrc).first, state, input, K - 1); };
+				auto calcSrc_second = [&]() {b = calcSrc_tbb(input->nandGateInputs.at(nandSrc).second, state, input, K - 1); };
+				calcSrc_group.run(calcSrc_first);
+				calcSrc_group.run(calcSrc_second);
+				calcSrc_group.wait();
+				return !(a&&b);
+			}
 		}
 	}
 
-	std::vector<int> next_int(const std::vector<int> &state, const puzzler::CircuitSimInput *input) const
+	//std::vector<bool> next_tbb(const std::vector<bool> &state, const puzzler::CircuitSimInput *input) const
+		//{
+		//	std::vector<bool> res(state.size());
+		//	/*for (unsigned i = 0; i<res.size(); i++){
+		//	res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		//	}*/
+
+		//	//if (res.size() < 1000){
+		//		int K = 1000;
+		//		typedef tbb::blocked_range<unsigned> my_range_t;
+		//		my_range_t range(0, res.size(), K);
+		//		auto f = [&](const my_range_t &chunk)
+		//		{
+		//			for (unsigned i = chunk.begin(); i != chunk.end(); i++)
+		//			{
+		//				res[i] = calcSrc_tbb(input->flipFlopInputs[i], state, input, res.size());
+		//			}
+		//		};
+		//		tbb::parallel_for(range, f, tbb::simple_partitioner());
+		//	//}
+		//	//else
+		//	//{
+		//	//	for (unsigned i = 0; i < res.size(); i++)
+		//	//	{
+		//	//		unsigned K = res.size();
+		//	//		res[i] = calcSrc_tbb(input->flipFlopInputs[i], state, input, K/4);
+		//	//		//res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		//	//	}
+		//	//}
+		//	/*unsigned size = res.size();
+		//	auto f = [&](unsigned i)
+		//	{
+		//		res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		//	};
+		//	tbb::parallel_for(0u, size, f);*/
+
+
+
+		//	/*for (unsigned i = 0; i < res.size(); i++)
+		//	{
+		//		res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		//	}*/
+
+		//	return res;
+		//}
+
+	std::vector<char> next(const std::vector<char> &state, const puzzler::CircuitSimInput *input) const
 	{
-		std::vector<int> res(state.size());
-		for (unsigned i = 0; i<res.size(); i++){
-			res[i] = calcSrc_int(input->flipFlopInputs[i], state, input);
+		std::vector<char> res(state.size());
+		for (unsigned i = 0; i < res.size()/2; i++){
+			res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+		}
+
+		for (unsigned i = res.size()/2; i < res.size(); i++){
+			res[i] = calcSrc(input->flipFlopInputs[i], state, input);
 		}
 
 		return res;
 	}
+
+	std::vector<char> next_tbb(const std::vector<char> &state, const puzzler::CircuitSimInput *input) const
+	{
+		std::vector<char> res(state.size());
+		int K = 100;
+		typedef tbb::blocked_range<unsigned> my_range_t;
+		my_range_t range(0, res.size(), K);
+		auto f = [&](const my_range_t &chunk)
+		{
+			for (unsigned i = chunk.begin(); i != chunk.end(); i++){
+				res[i] = calcSrc(input->flipFlopInputs[i], state, input);
+			}
+		};
+		tbb::parallel_for(range, f, tbb::simple_partitioner());
+		return res;
+	}
+
+
 public:
 	CircuitSimProvider()
 	{}
@@ -125,115 +177,68 @@ public:
 		) const override {
 		log->LogVerbose("About to start running clock cycles (total = %d", input->clockCycles);
 		std::vector<bool> state = input->inputState;
+		
+		//convert state to state_char
+		std::vector<char> state_char(input->inputState.size());
 
-		bool conversion = false;
-		bool parallel = true;
-		if (conversion == false)
+		for (unsigned i = 0; i < state_char.size(); i++)
 		{
-			if (parallel == true)	//Parallel outer loop
-			{
-				int K = 1000;
-				typedef tbb::blocked_range<unsigned> my_range_t;
-				my_range_t range(0, input->clockCycles, K);
-				auto f = [&](const my_range_t &chunk)
-				{
-					for (unsigned i = chunk.begin(); i != chunk.end(); i++)
-					{
-						log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
-
-						state = next_tbb(state, input);
-
-						// The weird form of log is so that there is little overhead
-						// if logging is disabled
-						log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
-							for (unsigned j = 0; i < state.size(); j++){
-								dst << state[j];
-							}
-						});
-					}
-				};
-				tbb::parallel_for(range, f, tbb::simple_partitioner());
-				//tbb::parallel_for(0u, input->clockCycles, f);
-			}
-			//else
-			//{	
-				
-				//for (unsigned i = 0; i<input->clockCycles/2; i++){
-				//	log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
-
-				//	state = next(state, input);
-
-				//	// The weird form of log is so that there is little overhead
-				//	// if logging is disabled
-				//	log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
-				//		for (unsigned i = 0; i<state.size(); i++){
-				//			dst << state[i];
-				//		}
-				//	});
-				//}
-				//for (unsigned i = input->clockCycles/2; i<input->clockCycles; i++){
-				//	log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
-
-				//	state = next(state, input);
-
-				//	// The weird form of log is so that there is little overhead
-				//	// if logging is disabled
-				//	log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
-				//		for (unsigned i = 0; i<state.size(); i++){
-				//			dst << state[i];
-				//		}
-				//	});
-				//}
-			//}
+			state_char[i] = (int)state[i];
 		}
-		//else
-		//{	//Convert from bool to int
-		//	std::vector<int> state_int;
-		//	for (bool value : state) {
-		//		state_int.push_back(value ? 1 : 0);
-		//	}
-		//	//conversion taken from reddit because of issues in VS with vector<bool>
 
-		//	//int K = 10;
-		//	//typedef tbb::blocked_range<unsigned> my_range_t;
-		//	//my_range_t range(0, input->clockCycles, K);
-		//	//auto f = [&](const my_range_t &chunk)
-		//	//{
-		//	//	for (unsigned i = chunk.begin(); i != chunk.end(); i++)
-		//	//	{
-		//	//		log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
+		for (unsigned i = 0; i < input->clockCycles; i++){
+			log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
 
-		//	//		state_int = next_int(state_int, input);
+			state_char = next_tbb(state_char, input);
 
-		//	//		// The weird form of log is so that there is little overhead
-		//	//		// if logging is disabled
-		//	//		log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
-		//	//			for (unsigned j = 0; i < state.size(); j++){
-		//	//				dst << state[j];
-		//	//			}
-		//	//		});
-		//	//	}
-		//	//};
-		//	//tbb::parallel_for(range, f, tbb::simple_partitioner());
+			// The weird form of log is so that there is little overhead
+			// if logging is disabled
+			log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
+				for (unsigned i = 0; i < state.size(); i++){
+					dst << state[i];
+				}
+			});
+		}
+		for (unsigned i = 0; i < state_char.size(); i++)
+		{
+			state[i] = (bool)state_char[i];
+		}
 
-		//	for (unsigned i = 0; i<input->clockCycles; i++){
+	//Parallel
+	{
+		//int K = 10;
+		//typedef tbb::blocked_range<unsigned> my_range_t;
+		//my_range_t range(0, input->clockCycles, K);
+		//auto f = [&](const my_range_t &chunk)
+		//{
+		//	std::vector<char> state_char(input->inputState.begin(), input->inputState.end());
+		//	
+		//	for (unsigned i = chunk.begin(); i != chunk.end(); i++)
+		//	{
 		//		log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
-
-		//		state_int = next_int(state_int, input);
+		//		fprintf(stderr, "Executing i: %d \n", i);
+		//		state_char = next(state_char, input);
 
 		//		// The weird form of log is so that there is little overhead
 		//		// if logging is disabled
 		//		log->Log(puzzler::Log_Debug, [&](std::ostream &dst) {
-		//			for (unsigned i = 0; i<state_int.size(); i++){
-		//				dst << state_int[i];
+		//			for (unsigned j = 0; i < state.size(); j++){
+		//				dst << state[j];
 		//			}
 		//		});
 		//	}
-
-		//	for (int i : state_int) {
-		//		state.at(i)=!!state_int.at(i);
+		//	int j = 0;
+		//	for (unsigned i = chunk.begin(); i != chunk.end(); i++)
+		//	{
+		//		state[i] = (bool)state_char[i];
+		//		j++;
 		//	}
-		//}
+		//};
+		//tbb::parallel_for(range, f, tbb::simple_partitioner());
+
+
+		//std::vector<bool> state_bool (state_char.begin(), state_char.end());
+	}
 		log->LogVerbose("Finished clock cycles");
 
 		output->outputState = state;
